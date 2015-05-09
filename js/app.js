12cae93225db3981b849a06e06b0326a638b4e9b
George <gkanch@gmail.com>
2015-05-08 20:28:45 -0700

    Ready for multiple cats
    Load multiple cat info

--- js/app.js
+++ js/app.js
@@ -1,12 +1,68 @@
+/* Cat data */
+var initialCats = [
+	{
+		imgSrc: 'img/cat01.jpg',
+		name: 'Tabby',
+		clickCount: 0,
+		nicknames: [
+			{ name: 'Tabtab', id: 121 },
+			{ name: 'T-bone', id: 122 },
+			{ name: 'Mr. T', id: 123}
+		]
+	},
+	{
+		imgSrc: 'img/cat02.jpg',
+		name: 'Tiger',
+		clickCount: 0,
+		nicknames: [
+			{ name: 'Tigger', id: 221 }
+		]
+	},
+	{
+		imgSrc: 'img/cat03.jpg',
+		name: 'Tommy',
+		clickCount: 0,
+		nicknames: [
+			{ name: 'Zack', id: 321 }
+		]
+	},
+	{
+		imgSrc: 'img/cat04.jpg',
+		name: 'Lulu',
+		clickCount: 0,
+		nicknames: [
+			{ name: 'Luny', id: 421 }
+		]
+	},
+	{
+		imgSrc: 'img/cat05.jpg',
+		name: 'Bat',
+		clickCount: 0,
+		nicknames: [
+			{ name: 'Batty', id: 521 }
+		]
+	},
+	{
+		imgSrc: 'img/cat06.jpg',
+		name: 'Doze',
+		clickCount: 0,
+		nicknames: [
+			{ name: 'Dozy', id: 621 }
+		]
+	},
+]
+
+/* Cat Constructor */
 var Cat = function(data) {
 	
 	// *** Observables & Observable Array ***
-	this.clickCount = ko.observable(data.clickCount);
 	this.name = ko.observable(data.name);
 	this.imgSrc = ko.observable(data.imgSrc);
-	this.imgAttribution = ko.observable(data.imgAttribution);
+	//this.imgAttribution = ko.observable(data.imgAttribution);
+	this.clickCount = ko.observable(data.clickCount);
 	this.nicknamesArray = ko.observableArray(data.nicknames);
 	
+	// *** title ***
 	this.title = ko.computed(function() {
 		var title = null;
 		var clicks = this.clickCount();
@@ -27,20 +83,17 @@ var Cat = function(data) {
 	}, this);
 }
 
+/* ViewModel */
 var ViewModel = function() {
 	var self = this; /* self points to ViewModel */
 	
-	this.currentCat = ko.observable( new Cat({
-		clickCount: 0,
-		name: 'Tabby',
-		imgSrc: 'img/434164568_fea0ad4013_z.jpg',
-		imgAttribution: 'https://www.flickr.com/...',
-		nicknames: [
-			{ name: 'Tabtab', id: 121 },
-			{ name: 'T-bone', id: 122 },
-			{ name: 'Mr. T', id: 123}
-		]
-	}) );
+	this.catList = ko.observableArray([]);
+	
+	initialCats.forEach(function(catItem) {
+		self.catList.push( new Cat(catItem) );
+	});
+	
+	this.currentCat = ko.observable( this.catList()[0] );
 	
 	this.incrementCounter = function() {
 		self.currentCat().clickCount(self.currentCat().clickCount() + 1);
